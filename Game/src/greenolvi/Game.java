757867fc4b7a2588a17a49e3ce39cdddd5c74324
Piotrek Szulc <szulo123@gmail.com>
2011-12-21 17:12:57 +0100

    Initial class

--- Game/src/greenolvi/Game.java
+++ Game/src/greenolvi/Game.java
@@ -1,13 +1,124 @@
 package greenolvi;
 
-public class Game {
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-	/**
-	 * @param args
-	 */
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String NAME = "Unknown";
+	public static final int HEIGHT = 240;
+	public static final int WIDTH = 320;
+	public static final int SCALE = 2;
+	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Game game = new Game();
+		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 
+		JFrame frame = new JFrame(Game.NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(game, BorderLayout.CENTER);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+		game.start();
+	}
+
+	private boolean running;
+
+	public void start() {
+		running = true;
+		new Thread(this).start();
+	}
+
+	public void stop() {
+		running = false;
+	}
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double unprocessed = 0;
+		double nsPerTick = 1000000000.0 / 60;
+		int frames = 0;
+		int ticks = 0;
+		long lastTimer1 = System.currentTimeMillis();
+		
+		init();
+		
+		while (running) {
+			long now = System.nanoTime();
+			unprocessed += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+			while (unprocessed >= 1) {
+				ticks++;
+				tick();
+				unprocessed -= 1;
+				shouldRender = true;
+			}
+			
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if (shouldRender) {
+				frames++;
+				render();
+			}
+			
+			if (System.currentTimeMillis() - lastTimer1 > 1000) {
+				lastTimer1 += 1000;
+				System.out.println(ticks + " ticks, " + frames + " fps");
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+
+	private void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			requestFocus();
+			return;
+		}
+		
+		Graphics g = getGraphics();
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		int ww = WIDTH * 3;
+		int hh = HEIGHT * 3;
+		int xo = (getWidth() - ww) / 2;
+		int yo = (getHeight() - hh) / 2;
+		g.drawImage(image, xo, yo, ww, hh, null);
+		g.dispose();
+		bs.show();
+	}
+
+	private void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
