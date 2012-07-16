52628d073db490352299fab9780eb5852a60c2fb
Piotr Szulc <szulo123@gmail.com>
2012-07-16 23:14:46 +0200

    Renamed package

---
+++ Game/src/game/Game.java
@@ -0,0 +1,145 @@
+package game;
+
+import game.gfx.Screen;
+import game.gfx.SpriteSheet;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+
+	public static final String NAME = "Unknown";
+	public static final int HEIGHT = 240;
+	public static final int WIDTH = 360;
+	public static final int SCALE = 2;
+
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	
+	private Screen screen;
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
+		JFrame frame = new JFrame(Game.NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(game, BorderLayout.CENTER);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+		game.start();
+	}
+
+	private boolean running;
+
+	public void start() {
+		running = true;
+		new Thread(this).start();
+	}
+
+	public void stop() {
+		running = false;
+	}
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double unprocessed = 0;
+		double nsPerTick = 1000000000.0 / 60;
+		int frames = 0;
+		int ticks = 0;
+		long lastTimer1 = System.currentTimeMillis();
+
+		init();
+
+		while (running) {
+			long now = System.nanoTime();
+			unprocessed += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+			while (unprocessed >= 1) {
+				ticks++;
+				tick();
+				unprocessed -= 1;
+				shouldRender = true;
+			}
+
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if (shouldRender) {
+				frames++;
+				render();
+			}
+
+			if (System.currentTimeMillis() - lastTimer1 > 1000) {
+				lastTimer1 += 1000;
+				System.out.println(ticks + " ticks, " + frames + " fps");
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+
+	private void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			requestFocus();
+			return;
+		}
+
+		screen.renderFractal();
+		screen.render(100, 100, 0);
+		
+		for (int y = 0; y < screen.height; y++) {
+			for (int x = 0; x < screen.width; x++) {
+				pixels[x + y * WIDTH] = screen.pixels[x + y * WIDTH];
+			}
+		}
+
+		Graphics g = bs.getDrawGraphics();
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		int ww = WIDTH * SCALE;
+		int hh = HEIGHT * SCALE;
+		int xo = (getWidth() - ww) / 2;
+		int yo = (getHeight() - hh) / 2;
+		g.drawImage(image, xo, yo, ww, hh, null);
+		g.dispose();
+		bs.show();
+	}
+
+	private void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void init() {
+		try {
+			this.screen = new Screen(WIDTH, HEIGHT, new SpriteSheet(ImageIO.read(Game.class.getResourceAsStream("/icons.png"))));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Game/src/game/gfx/Screen.java
@@ -0,0 +1,45 @@
+package game.gfx;
+
+public class Screen {
+	
+	public final int width, height;
+	public int[] pixels;
+	
+	private SpriteSheet sheet;
+
+	public Screen(int width, int height, SpriteSheet sheet) {
+		this.sheet = sheet;
+		this.width = width;
+		this.height = height;
+		
+		pixels = new int[width * height];
+	}
+
+	public void clear(int color) {
+		for (int i = 0; i < pixels.length; i++)
+			pixels[i] = color;
+	}
+	
+	public void renderFractal() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int draw = 1;
+				if ((x & y) > 0)
+					draw = 0;
+				pixels[y * width + x] = (draw * x) << 16 | (draw * y) << 8 | (draw);
+			}
+		}	
+	}
+	
+	public void render(int xp, int yp, int tile) {
+		int xTile = tile % 32;
+		int yTile = tile / 32;
+		int toffs = xTile * 8 + yTile * 8 * sheet.width;
+		
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				pixels[(x + xp) + (y + yp) * width] = sheet.pixels[x + y * sheet.width + toffs];
+			}
+		}
+	}
+}
